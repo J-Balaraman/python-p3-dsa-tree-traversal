27212685be51e2d2f15d9cd2dced630dd4a3d75b
jbalaraman <james.balaraman@gmail.com>
2023-12-28 16:42:25 -0700

    practices tree transversal

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,12 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+      nodes_to_visit = [self.root]
+    
+      while len(nodes_to_visit) > 0:
+        if nodes_to_visit[0]['id'] == id:
+           return nodes_to_visit[0]
+        node = nodes_to_visit.pop(0)
+        nodes_to_visit = node['children'] + nodes_to_visit
+    
+      return None
